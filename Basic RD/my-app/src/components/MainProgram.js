224ca4ba034d20f28aaabe9b457a86004b58e14d
VEERAWAT-MEC\Veerawat <cloud-strife-pom@hotmail.com>
2022-11-21 15:58:05 +0700

    finish

--- Basic RD/my-app/src/components/MainProgram.js
+++ Basic RD/my-app/src/components/MainProgram.js
@@ -1,5 +1,17 @@
 import React, { useEffect, useState } from "react";
-
+function isPalindrome(s) {
+    let isPalindrome = false;
+    let inverted_s = "";
+  
+    for (let index = s.length - 1; index >= 0; index--) {
+      inverted_s = inverted_s + s[index];
+    }
+    //console.log(inverted_s);
+    if (inverted_s == s) {
+      isPalindrome = true;
+    }
+    return isPalindrome;
+  }
 const MainProgram = (props) => {
   const [inputText, setInputText] = useState("start val");
   const [outputText, setOutputText] = useState("กรุณาพิมพ์ และกดปุ่ม");
@@ -39,14 +51,61 @@ const MainProgram = (props) => {
   };
 
   const checkingInputVal = () => {
-    //setIsLoading(true);
-    const thisInputText = inputText;
-    if (thisInputText === "done") {
-      setOutputText("รัน checkingInputVal finish");
-    } else {
-      setOutputText("กรุณา done และกดปุ่ม");
+    let s = inputText;
+    let longest = 0;
+    let longestSubString = "";
+    let left = 0;
+    let right = 0;
+    if (s.length === 1) {
+      longest = 1;
+      longestSubString = s;
     }
-    //setIsLoading(false);
+    if (s.length === 2) {
+      if (s[0] === s[1]) {
+        longest = 2;
+        longestSubString = s;
+      }
+    }
+    if (s.length > 2) {
+      if (s[s.length - 1] === s[s.length - 2]) {
+        longest = 2;
+        longestSubString = s[s.length - 1] + s[s.length - 2];
+      }
+    }
+    if (isPalindrome(s) && s.length > 1) {
+      longest = s.length;
+      longestSubString = s;
+    } else {
+      for (let index = 1; index < s.length; index++) {
+        left = index - 1;
+        right = index + 1;
+        if (s[index] === s[right]) {
+          if (longest < 2) {
+            longest = right - index + 1;
+            longestSubString = s.substring(index, right + 1);
+          }
+        }
+        do {
+          if (s[left] === s[right]) {
+            if (right - left + 1 > longest) {
+              longest = right - left + 1;
+              longestSubString = s.substring(left, right + 1);
+            }
+          } else {
+            break;
+          }
+          left--;
+          right++;
+        } while (left >= 0 && right <= s.length);
+      }
+    }
+    if (longest === 0) {
+      setOutputText("No Palindromic");
+    } else {
+      setOutputText(
+        "Longest Palindromic = " + longest + " Value = " + longestSubString
+      );
+    }
   };
 
   return (
