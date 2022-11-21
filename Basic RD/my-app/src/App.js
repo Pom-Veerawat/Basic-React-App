6c046300eeae5194d19c01f0efe2294483d3dfe5
VEERAWAT-MEC\Veerawat <cloud-strife-pom@hotmail.com>
2022-11-21 12:44:59 +0700

    add Card

--- Basic RD/my-app/src/App.js
+++ Basic RD/my-app/src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './UI/Card';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <div>TEST</div>
+        <Card title={"Titlessss"} />
       </header>
     </div>
   );
