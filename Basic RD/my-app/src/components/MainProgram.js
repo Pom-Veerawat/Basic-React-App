78358d129f46f78e752925b3a55ae71a5718a54d
VEERAWAT-MEC\Veerawat <cloud-strife-pom@hotmail.com>
2022-11-21 15:06:00 +0700

    add function and use effect

--- Basic RD/my-app/src/components/MainProgram.js
+++ Basic RD/my-app/src/components/MainProgram.js
@@ -1,29 +1,52 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 const MainProgram = (props) => {
   const [inputText, setInputText] = useState("start val");
-const [outputText,setOutputText]= useState("กรุณาพิมพ์ และกดปุ่ม");
+  const [outputText, setOutputText] = useState("กรุณาพิมพ์ และกดปุ่ม");
 
+  useEffect(() => {
+    
+    console.log("EFFECT RUNNING");
+    checkingInputVal();
+    return() =>{
+        console.log("EFFECT CLEANUP");
+    }
+
+  }, [inputText]);
 
   const onButtonClickHandler = () => {
     console.log("clicked!!");
     console.log(inputText);
+    checkingInputVal();
   };
 
-  const inputOnchangeHandler=(event)=>{
-    setInputText((prev)=>{
-        //prev การันตีค่าก่อนหน้า
-       return event.target.value;
-    })
-  }
+  const inputOnchangeHandler = (event) => {
+    setInputText((prev) => {
+      //prev การันตีค่าก่อนหน้า
+      return event.target.value;
+    });
+  };
+
+  const checkingInputVal = () => {
+    const thisInputText = inputText;
+    if (thisInputText === "done") {
+      setOutputText("รัน checkingInputVal finish");
+    } else {
+      setOutputText("กรุณา done และกดปุ่ม");
+    }
+  };
 
   return (
     <div>
-      <input style={{ width: "500px" }} onChange={inputOnchangeHandler} value={inputText} type="text"></input>
+      <input
+        style={{ width: "500px" }}
+        onChange={inputOnchangeHandler}
+        value={inputText}
+        type="text"
+      ></input>
       <br></br>
       <button onClick={onButtonClickHandler}>Click</button>
-      <br>
-      </br>
+      <br></br>
       <p>{outputText}</p>
     </div>
   );
