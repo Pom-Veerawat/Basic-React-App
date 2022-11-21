26d12cd41e370aac03de5856b03fc7676f329e4a
VEERAWAT-MEC\Veerawat <cloud-strife-pom@hotmail.com>
2022-11-21 15:37:52 +0700

    lift state up

--- Basic RD/my-app/src/App.js
+++ Basic RD/my-app/src/App.js
@@ -2,15 +2,26 @@ import logo from './logo.svg';
 import './App.css';
 import Card from './UI/Card';
 import MainProgram from './components/MainProgram';
+import { useState } from 'react';
 
 function App() {
+
+  const [title,setTitle]= useState("initial");
+
+  const onReturnFromMainProgram = (val)=>{
+    setTitle(val);
+  }
+
+
+  
+
   return (
     <div className="App">
       <header className="App-header">
-        <div>TEST</div>
+        <div>TEST and {title}</div>
         <Card>
-          <MainProgram>
-            
+          <MainProgram onTitleChange={onReturnFromMainProgram}>
+
           </MainProgram>
         </Card>
       </header>
